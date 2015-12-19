c089666bb020cf193a894b685c4940ec390d4c90
andiruda <andiruda@gmail.com>
2015-12-18 17:37:13 -0800

    Adding Automated Git Pull script

---
+++ update.php
@@ -0,0 +1,9 @@
+<?php
+
+  //exec("git pull https://andiruda:rmr2056IAR!13@github.com/andiruda/therudafamilysite master",$response,$response2);
+  exec("git pull 2>&1",$response);
+  //print_r($response);
+  //exec("whoami 2>&1",$response);
+
+?>
+  <h2><?php echo $response[0] ?></h2>
