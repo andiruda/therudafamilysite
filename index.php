2d236a97ed9782a1ecdd5caca4cfb00f8512af31
andiruda <andiruda@gmail.com>
2015-12-18 16:54:39 -0800

    Fix image targeting on index page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 	<h1>The Ruda Family Blog</h1>
-	<img class="widthSpanImg right" src="/images/isaac-standing-YWAM-sign-2.jpg"  />
+	<img class="widthSpanImg right" src="http://therudafamily.com/images/isaac-standing-YWAM-sign-2.jpg"  />
 	<p itemscope itemtype="http://data-vocabulary.org/Person">
-		Welcome to <span itemprop="name">The Ruda Family</span> Blog. Our story is a unique weave of toxic mold, living tiny and our faith moving us into Missions. This blog is a combination of the three, mainly because that’s who we are. We can’t really separate them. 
+		Welcome to <span itemprop="name">The Ruda Family</span> Blog. Our story is a unique weave of toxic mold, living tiny and our faith moving us into Missions. This blog is a combination of the three, mainly because that’s who we are. We can’t really separate them.
 	</p>
 	<p>
 		This story will start with Toxic Mold, but if you are dealing with it yourself, just remember our story doesn’t end there. We have had many people tell us to "write a book" about our recent episode with Toxic Mold.  We haven’t been able to do that yet but this is our start.  We Hope to have encouraging as well as helpful information in the mold help section.
@@ -10,7 +10,7 @@
 		Once we started healing from the mycotoxins from the mold, we moved into a tiny home so that we could make sure we had no recurrences.  We had to simplify our diet, our finances, and our living space in order to heal. Simplicity is a kindness to the soul.  Therefore, the tiny house movement has made us smile. If you are interested in living tiny please feel free to contact us, we love chatting about living small.  It’s been so good to us.
 	</p>
 	<p>
-		After our healing really started to take root…life just became clearer. Priorities lined up easily. Pain has a funny way of making our Faith obvious to ourselves.   When you face a darkness that  lies about it's origin you begin to question whether or not it really was from God. But then on the other side of it all...it becomes so clear.  Missions for us is simply the next step.  God is Good. He is so kind. So, if you would like to hear more on that, this is where we will tell that side of the story.  
+		After our healing really started to take root…life just became clearer. Priorities lined up easily. Pain has a funny way of making our Faith obvious to ourselves.   When you face a darkness that  lies about it's origin you begin to question whether or not it really was from God. But then on the other side of it all...it becomes so clear.  Missions for us is simply the next step.  God is Good. He is so kind. So, if you would like to hear more on that, this is where we will tell that side of the story.
 	</p>
 	<span  class="articlesDate">Rachel</span>
 
@@ -22,7 +22,7 @@
 </div>
 
 <script>
-		
+
 	//Cookie Cutter Prototype
 		var cookieProto = {
 			create: function(key,val,exp) {
@@ -30,12 +30,12 @@
 				var cKey = key||"dsPOP";
 				var cVal = val||"true";
 				var cExp = exp || .5;
-				
+
 				//Set expiration
 				var d = new Date();
 				d.setTime(d.getTime() + (cExp*24*60*60*1000));
 				cExp = "expires="+d.toGMTString();
-				
+
 				//set the cookie
 				document.cookie = cKey + "=" + cVal + "; " + cExp;
 			},
@@ -43,7 +43,7 @@
 				var cKey = key;
 				var cAry = document.cookie.split("; ");
 				for(x in cAry){
-					var valAry = cAry[x].split("=");	
+					var valAry = cAry[x].split("=");
 					if(valAry[0]==cKey){
 						var c = cAry[x].trim();
 						cKey = cKey + "=";
@@ -69,8 +69,8 @@
 				}
 			}
 		}
-		
-	
+
+
 		//PopUp ON Exit
 		var usrCookie = Object.create(cookieProto);
 </script>
@@ -81,5 +81,5 @@
 		//$.colorbox({html:"<div id='newsletterSignup'><iframe width='300' height='600' src='http://eepurl.com/Zrc69'></iframe></div>"})
 		usrCookie.create("popUp","true");
 	}
-	
+
 </script>
